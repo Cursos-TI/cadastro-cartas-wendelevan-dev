51898806bec1034919af13cf1ce863101c2476e7
wendelevan-dev <wendelevan65@gmail.com>
2025-11-02 00:09:19 +0000

    começo

---
+++ Países.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("Super Trunfo, nível novato!");
+    
+    char[20]A01, B01;       
+    char[20]Cidade;  
+    int População;     
+    float Área em km^2;     
+    float PIB;           
+    int Pontos Turísticos;
+
+    return 0;
+
+
+
+
+}
